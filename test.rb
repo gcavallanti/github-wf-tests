02fdd22b7dc3d274f84371eab9b4998a93e74171
Giovanni Cavallanti <gcavn@gcavn.com>
2014-09-14 16:46:14 +0200

    Update test.rb

--- test.rb
+++ test.rb
@@ -1,24 +1,4 @@
-class A < B; def self.create(object = User) object end end
-class Zebra; def inspect; "X#{2 + self.object_id}" end end
-
-module ABC::DEF
-  include Comparable
-
-  # @param test
-  # @return [String] nothing
-  def foo(test)
-    Thread.new do |blockvar|
-      ABC::DEF.reverse(:a_symbol, :'a symbol', :<=>, 'test' + test)
-    end.join
-  end
-
-  def [](index) self[index] end
-  def ==(other) other == self end
-end
-
-anIdentifier = an_identifier
-Constant = 1
-render action: :new
+require 'active_support'
 
 
 module VimColors
@@ -56,3 +36,26 @@ module VimColors
     end
   end
 end
+
+class A < B; def self.create(object = User) object end end
+class Zebra; def inspect; "X#{2 + self.object_id}" end end
+
+module ABC::DEF
+  include Comparable
+
+  # @param test
+  # @return [String] nothing
+  def foo(test)
+    Thread.new do |blockvar|
+      ABC::DEF.reverse(:a_symbol, :'a symbol', :<=>, 'test' + test)
+    end.join
+  end
+
+  def [](index) self[index] end
+  def ==(other) other == self end
+end
+
+anIdentifier = an_identifier
+Constant = 1
+render action: :new
+
